339fc627b903598299a283febab180a7ccef6bbd
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2010-10-29 19:50:11 +0200

    Remove spec/spec_helper.rb

--- spec/swinger_spec.rb
+++ spec/swinger_spec.rb
@@ -1,4 +1,7 @@
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'rspec'
+require 'capybara'
 
 describe RSpec::Core::Example do
 
